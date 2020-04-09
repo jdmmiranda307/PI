0d97ea6e9258c1e11c79eb932825c7d4bed56345
João Miranda <jvdmmiranda@gmail.com>
2020-04-09 17:41:09 -0300

    Create new apps

---
+++ professor/urls.py
@@ -0,0 +1,9 @@
+# from django.conf.urls import url
+# from django.contrib.auth import views as auth_views
+
+# from core import views
+
+# urlpatterns = [
+#     url(r'^users/login/$', views.login_caller, name='login'),
+#     url(r'^users/logout/$', auth_views.logout, name='logout'),
+# ]
