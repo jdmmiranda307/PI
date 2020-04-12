ab18b47fe86117bf656f6d72e5dec7b17b31856b
João Miranda <jvdmmiranda@gmail.com>
2020-04-12 20:40:59 -0300

    Adds new classes to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from core.models import Aluno, Aula
+from core.models import *
 
 # Register your models here.
 admin.site.register(Aluno)
 admin.site.register(Aula)
+admin.site.register(Curso)
+admin.site.register(Disciplina)
+admin.site.register(Professor)
+admin.site.register(CursoDisciplina)
+admin.site.register(AulaAluno)
